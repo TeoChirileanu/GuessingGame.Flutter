855cd5b5fe5334a70e8764aab25877f14d732a68
Teodor <teodorchirileanu@gmail.com>
2021-01-26 20:25:34 +0100

    primitive alert dialog echo-ing user input

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "GuessingGame",
+      debugShowCheckedModeBanner: false,
+      home: GuessedNumberForm(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class GuessedNumberForm extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GuessedNumberFormState createState() => _GuessedNumberFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GuessedNumberFormState extends State<GuessedNumberForm> {
+  final textEditingController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("Wanna play a game?"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Enter your guess:"),
+          controller: textEditingController,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text("Your guess: ${textEditingController.text}"),
+              );
+            },
+          );
+        },
+        tooltip: "Click to check your guess!",
+        child: Icon(Icons.check_circle),
       ),
     );
   }
